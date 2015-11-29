296fc92f0814ec772f1c61cb9f14a123804b3c67
David Helkowski <nanoscopic@users.noreply.github.com>
2015-11-28 17:13:42 -0800

    Remove copy of Class::Core; use the public one

--- Class/t/Basic.t
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl -w
-
-use strict;
-
-use Test::More qw(no_plan);
-
-use_ok( 'Class::Core' );
-
--- Class/t/hash.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl -w
-use strict;
-use hash;
-my $t = new hash;
-$t->runhash( path => 'fsum.exe' );
